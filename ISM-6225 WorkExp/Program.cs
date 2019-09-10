6fd83e3a3bdf595480ff1bae621cd44d857fc02b
coezema <49453273+coezema@users.noreply.github.com>
2019-09-10 19:49:33 -0400

    Create Program.cs

---
+++ ISM-6225 WorkExp/Program.cs
@@ -0,0 +1,46 @@
+﻿/*	Name: Ebby Ezema
+	Date: 09/10/2019
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISM_6225_WorkExp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("How many years of exp do you have of professional programming?");
+				string input = Console.ReadLine();
+				decimal exp = decimal.Parse(input);
+
+				if (exp > 2)
+				{
+					Console.WriteLine("Youre an expert!");
+				}
+				else if (exp == 2)
+				{
+					Console.WriteLine("Wow! You've been doing this for a little while");
+				}
+
+				else if (exp == 1)
+				{
+					Console.WriteLine("Looks like you have a little experience");
+				}
+				else if(exp == 2)
+				{
+					Console.WriteLine("Need lots of practice");
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Please use decimals");
+			}
+		}
+	}
+}
