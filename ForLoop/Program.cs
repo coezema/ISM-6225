c89ef394e6660d153e9968abdd06678cb5f1a608
coezema <49453273+coezema@users.noreply.github.com>
2019-09-10 21:00:21 -0400

    For Loop initial commit

---
+++ ForLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Years of exp?");
+			string input = Console.ReadLine();
+			int exp = int.Parse(input);
+
+			for (int i = 0; i <= exp;i++)
+			{
+				if (i < 1)
+				{
+					Console.WriteLine("You have " + i + " year of experience");
+				}
+				else
+				{
+					Console.WriteLine("You have " + i + " years of experience");
+				}
+			}
+			Console.Write("Please press key to end program");
+			Console.ReadKey(true);
+
+		}
+	}
+}
