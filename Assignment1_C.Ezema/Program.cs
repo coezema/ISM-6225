0108ccc1420fd98570f8286e3a20b1d38f34576e
coezema <49453273+coezema@users.noreply.github.com>
2019-09-16 22:46:16 -0400

    Q4 completed

--- Assignment1_C.Ezema/Program.cs
+++ Assignment1_C.Ezema/Program.cs
@@ -16,24 +16,26 @@ namespace Assignment1_C.Ezema
 			int n2 = 5;
 			printSeries(n2);
 
-			/*int n3 = 5;
+			int n3 = 5;
 			printTriangle(n3);
 
+
+			Console.WriteLine("\n\nQuestion 4\n\nJewels Count:");
 			int[] J = new int[] { 1, 3 };
 			int[] S = new int[] { 1, 3, 3, 2, 2, 2, 2, 2 };
 			int r4 = numJewelsInStones(J, S);
 			Console.WriteLine(r4);
 
+			Console.WriteLine("\n\nQuestion 5\n\nLargest Common Array:");
 			int[] arr1 = new int[] { 1, 2, 5, 6, 7, 8, 9 };
 			int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
 			int[] r5 = getLargestCommonSubArray(arr1, arr2);
 			Console.WriteLine(r5);
+			Console.WriteLine("\n\nPlease press key to end program");
+			Console.ReadKey(true);
 
 			solvePuzzle();
-			*/
 		}
-
-		// 
 		static bool isSelfDividing(int n)
 		{
 			int num = n;
@@ -45,7 +47,7 @@ namespace Assignment1_C.Ezema
 			else
 			{
 				while (num > 0)
-				{ 
+				{
 					int digit = num % 10;
 
 					if (n % digit != 0)
@@ -62,9 +64,10 @@ namespace Assignment1_C.Ezema
 			}
 		}
 		public static void printSelfDividingNumbers(int x, int y)
-		{
-			try
-			{
+        {
+            try
+            {
+				// Write your code here
 				if (x > y)
 				{
 					int a = x;
@@ -76,53 +79,175 @@ namespace Assignment1_C.Ezema
 					// do nothing
 				}
 				Console.WriteLine("Question 1\n");
-				Console.WriteLine("The self-dividing number(s) between "+x.ToString()+" & "+ y.ToString()+" are:");
+				Console.WriteLine("The self-dividing number(s) between " + x.ToString() + " & " + y.ToString() + " :");
 				while (x <= y)
 				{
 					if (isSelfDividing(x))
 					{
 						Console.Write(x.ToString() + " ");
-					}						
+					}
 					else
 					{
 						// do nothing
 					}
-					x++;	
+					x++;
 				}
 			}
-			catch
-			{
-				Console.WriteLine("Exception occured while computing printSelfDividingNumbers()");
-			}
-		Console.WriteLine("\n\n\nQuestion 2\n");
-		}
-		public static void printSeries(int n)
-		{
-			/*
-			* n – number of lines for the pattern, integer (int)
-			* 
-			* summary      : This method prints an inverted triangle using *
-			* For example n = 5 will display the output as: 
-			*********
-			 *******
-			  *****
-			   ***
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printSelfDividingNumbers()");
+            }
+        }
+
+        public static void printSeries(int n)
+        {
+            try
+            {
+				// Write your code here
+				Console.WriteLine("\n\n\nQuestion 2\n");
+				/*
+				* n – number of terms of the series, integer (int)
+				* 
+				* summary        : This method prints the following series till n terms:
+				* 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6 …. n terms
+				* For example, if n = 5, output will be
+				* 1, 2, 2, 3, 3
 				*
-
-			*
-			* returns      : N/A
-			* return type  : void
-			*/
-			Console.WriteLine("Inverted triangle series:\n");
-			while (n > 0)
-			{
-				string invTri = new string('*', n);
-				Console.WriteLine(invTri);
-				n--;
+				* returns        : N/A
+				* return type    : void
+				*/
+				Console.WriteLine("Print series:\n");
+				string ans = "";
+				int i = 1;
+				while (ans.Length < n)
+				{
+					string a = i.ToString();
+					string t1 = new string(char.Parse(a), i);
+					ans = ans + t1;
+					i++;
+				}
+				if (ans.Length > n)
+				{
+					int rem = ans.Length - n;
+					ans = ans.Substring(0, ans.Length - rem);
+				}
+				else
+				{
+					//;
+				}
+				Console.Write(ans);
 			}
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printSeries()");
+            }
+        }
 
-			Console.WriteLine("\n\nPlease press key to end program");
-			Console.ReadKey(true);
-		}
-	}
-}
+        public static void printTriangle(int n)
+        {
+            try
+            {
+				// Write your code here
+				Console.WriteLine("\n\n\nQuestion 3\n");
+				/*
+				* n – number of lines for the pattern, integer (int)
+				* 
+				* summary      : This method prints an inverted triangle using *
+				* For example n = 5 will display the output as: 
+				*********
+				 *******
+				  *****
+				   ***
+					*
+
+				*
+				* returns      : N/A
+				* return type  : void
+				*/
+				Console.WriteLine("Inverted triangle series:\n");
+				while (n > 0)
+				{
+					string invTri = new string('*', n);
+					Console.WriteLine(invTri);
+					n--;
+				}
+			}
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printTriangle()");
+            }
+        }
+
+        public static int numJewelsInStones(int[] J, int[] S)
+        {
+            try
+            {
+				/*
+				* a – array of elements, integer (int)
+				* 
+				* summary      : You're given an array J representing the types of stones that are 
+				* jewels, and S representing the stones you have.  Each element in S is a type of 
+				* stone you have.  You want to know how many of the stones you have are also jewels.
+				* The elements in J are guaranteed distinct.
+				* The function should return an integer.
+				* For example:
+				* J = [1,3], S = [1,3,3,2,2,2,2,2] will return the output: 
+				* 3 (since 1, 3, 3 are jewels)
+				* and
+				* J = [2], S = [0,0] will return the output: 
+				* 0
+				*
+				* returns      : Integer
+				* return type  : int
+				*/
+
+				// Write your code here
+				int count = 0;
+				foreach (int stone in S)
+				{
+					if (J.Contains(stone))
+					{
+						count++;
+					}
+					else
+					{
+						//
+					}
+				}
+				return count;
+			}
+            catch
+            {
+                Console.WriteLine("Exception occured while computing numJewelsInStones()");
+            }
+
+            return 0;
+        }
+
+        public static int[] getLargestCommonSubArray(int[] a, int[] b)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing getLargestCommonSubArray()");
+            }
+
+            return null; // return the actual array
+        }
+
+        public static void solvePuzzle() 
+        {
+            try 
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing solvePuzzle()");
+            }
+        }
+    }
+} 
