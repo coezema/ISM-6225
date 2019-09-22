866bc87f6e1dd80891cffdedfcaa43f632534204
coezema <49453273+coezema@users.noreply.github.com>
2019-09-22 17:11:45 -0400

    Solved UBER +COOL= UNCLE

--- Assignment1_C.Ezema/Program.cs
+++ Assignment1_C.Ezema/Program.cs
@@ -31,10 +31,10 @@ namespace Assignment1_C.Ezema
 			int[] arr2 = new int[] { 1, 2, 5, 6, 7, 8, 9 };
 			int[] r5 = getLargestCommonSubArray(arr1, arr2);
 			displayArray(r5);
-			Console.WriteLine("\n\nPlease press key to end program");
-			Console.ReadKey(true);
 
 			solvePuzzle();
+			Console.WriteLine("\n\nPlease press key to end program");
+			Console.ReadKey(true);
 		}
 		static bool isSelfDividing(int n)
 		{
@@ -302,13 +302,115 @@ namespace Assignment1_C.Ezema
             return null; // return the actual array
         }
 
-        public static void solvePuzzle() 
+		public static string GetWords(string input, string input2, string output)
+		{
+			Console.Write("Type first word and press Enter: ");
+			input = Console.ReadLine();
+			Console.Write("Enter second word and Press Enter: ");
+			input2 = Console.ReadLine();
+			Console.Write("Enter output word and press Enter: ");
+			output = Console.ReadLine();
+			string texts = string.Format("{0}{1}{2}", input, input2, output);
+
+			return texts;
+		}
+		// Taken from https://www.geeksforgeeks.org/numbers-unique-distinct-digits/ and modified for my needs here
+		// Method to print unique digit numbers 
+		// in range from l to r. 
+		public static int [] UniqueNumber(int l, int r)
+		{
+			List<int> NumList = new List<int>();
+			// Start traversing the numbers 
+			for (int i = l; i <= r; i++)
+			{
+				int num = i;
+				bool[] visited = new bool[10];
+
+				// Find digits and maintain 
+				// its hash 
+				while (num != 0)
+				{
+
+					// if a digit occcurs more 
+					// than 1 time then break 
+					if (visited[num % 10])
+						break;
+
+					visited[num % 10] = true;
+
+					num = num / 10;
+				}
+
+				// num will be 0 only when 
+				// above loop doesn't get 
+				// break that means the number 
+				// is unique so print it. 
+				if (num == 0)
+				{
+					NumList.Add(i);
+				}
+					
+			}
+			return NumList.ToArray();
+		}
+
+		//taken from https://stackoverflow.com/questions/829174/is-there-an-easy-way-to-turn-an-int-into-an-array-of-ints-of-each-digit
+		public static int[] NumbersIn(int value)
+		{
+			var numbers = new Stack<int>();
+
+			for (; value > 0; value /= 10)
+				numbers.Push(value % 10);
+
+			return numbers.ToArray();
+		}
+		public static void solvePuzzle() 
         {
             try 
             {
-                // Write your code here
-            }
-            catch
+				// Write your code here
+				/*use maximal organization of your
+				* code into the appropriate methods(e.g.a method to collect inputs, a method to
+				* identify unique characters in the strings, a method to test the currently assigned
+				* values etc).*/
+
+
+				string words =(GetWords(Console.ReadLine(), Console.ReadLine(), Console.ReadLine()));
+				string answer = new String(words.Distinct().ToArray());
+				int l = 100000000, r = 999999999;
+				int[] AlphatoNum = UniqueNumber(l, r);
+				int[] test = new int[] { };
+				foreach (int element in AlphatoNum)
+				{
+					int [] digits = NumbersIn(element);
+					int U = digits[0];
+					int B = digits[1];
+					int E = digits[2];
+					int R = digits[3];
+					int C = digits[4];
+					int O = digits[5];
+					int L = digits[6];
+					int N = digits[7];
+
+					int UBER = int.Parse(U.ToString() + B.ToString()+ E.ToString() + R.ToString());
+					int COOL = int.Parse(C.ToString() + O.ToString() + O.ToString() + L.ToString());
+					int UNCLE = int.Parse(U.ToString() + N.ToString() + C.ToString() + L.ToString() + E.ToString());
+
+					if ((UBER + COOL) == UNCLE)
+					{
+						Console.Write(UBER.ToString()+" "+ COOL.ToString()+"= " +UNCLE.ToString());
+						break;
+					}
+				}
+
+				Console.WriteLine("\n\nPlease press key to end program");
+				Console.ReadKey(true);
+				/*for (int i = 0; i < answer.Length;i++)
+				{
+				
+				}*/
+			}
+			catch
             {
                 Console.WriteLine("Exception occured while computing solvePuzzle()");
             }
