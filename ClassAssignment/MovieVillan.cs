d87303b04ddd9a74742cc8c093086081d149abe2
coezema <49453273+coezema@users.noreply.github.com>
2019-09-24 21:16:49 -0400

    Class Assignment

---
+++ ClassAssignment/MovieVillan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+	class MovieVillan: Person
+	{
+		public string signatureMove;
+		public int scareIndex;
+
+		public MovieVillan()
+		{
+			firstName = "Jason";
+			lastName = "Vorhees";
+			signatureMove = "Walking really slow";
+			scareIndex = 8;
+		}
+	}
+}
---
+++ ClassAssignment/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+	class Person
+	{
+		public string firstName;
+		public string lastName;
+		public string dateOfBirth;
+
+		public Person()
+		{
+			firstName = "James";
+			lastName = "Doe";
+			dateOfBirth = "1/1/1956";
+
+		}
+	}
+
+}
